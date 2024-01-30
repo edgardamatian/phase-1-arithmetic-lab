288debedadb6f5cb1c1f658be8af1636f0fcb47b
Edgar Damatian <damatian.e@gmail.com>
2024-01-29 23:09:42 -0800

    finally

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+console.log(multiply); // 62
+
+function calculateMod() {
+    let num1 = 8;
+    let num2 = 2;
+    let multiply = num1 * num2;
+    let remainder = 4;
+  
+    return remainder;
+  }
+
+  function max() {
+    let num1 = 10;
+    let num2 = 2;
+    let multiply = num1 * num2;
+    let remainder = 20;
+  
+    // Compare the values and return the maximum
+    let maxNumber = Math.max(num1, num2, multiply, remainder);
+    return maxNumber;
+  }
+  
+  // Test the max function
+  console.log(max()); // Output: 20
